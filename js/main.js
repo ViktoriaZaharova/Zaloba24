4cc664beadf40c634169e2fa8e574e10f7ff72b6
Viktoria <zaharova181114@gmail.com>
2021-10-13 15:24:21 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -26,3 +26,46 @@ $(".go_to").on("click", function (event) {
 	//анимируем переход на расстояние - top за 500 мс
 	$('body,html').animate({ scrollTop: top - 70 }, 500);
 });
+
+$(".category-section .problem__item").each(function () {
+	var count = $(this).find('.problem__list-item.hide').length;
+	var container = $(this);
+	$(this).find(".problem__more span").html(count);
+	$(this).find(".problem__more").on("click", function () {
+		count -= 6;
+
+		container.find(".problem__list-item.hide:lt(6)").removeClass('hide');
+		container.find(".problem__more span").html(count);
+		if (count <= 0) {
+			container.find(".problem__more").hide();
+		}
+		return false;
+	});
+});
+
+$('.links-toggle-text').on('click', function () {
+	$(this).parent().find('.statement-item').addClass('open-text');
+});
+
+$('.links-toggle-text').on('click', function (e) {
+	e.preventDefault();
+
+	var
+		$this = $(this),
+		content = $(this).parent().find('.statement-item');
+
+
+	if (!$this.hasClass('trigger')) {
+		$this.addClass('trigger');
+		$this.html('Скрыть ТЕКСТ ЗАЯВЛЕНИЯ');
+
+		content.addClass('open-text');
+	} else {
+		$this.removeClass('trigger');
+		$this.html('ПОЛНЫЙ ТЕКСТ ЗАЯВЛЕНИЯ');
+
+		content.removeClass('open-text');
+	}
+});
+
+$('.js-example-basic-single').select2({ width: '100%' });
